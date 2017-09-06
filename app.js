8a11a4b09ec6950792672d6a63d1618d6eaf26af
熊鑫 <xiongxin@xiongxindeMacBook-Pro.local>
2017-09-06 22:06:53 +0800

    add notes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// require 路由文件，可以看routes里面的文件怎么写的，也很简单
 var index = require('./routes/index');
 var users = require('./routes/users');
 var signin = require('./routes/signin');
@@ -13,11 +14,11 @@ var forgetpw=require('./routes/forgetpw');
 
 var app = express();
 
-// view engine setup
+// view engine setup 设置解析jade模版的引擎
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
+// uncomment after placing your favicon in /public  基本设置
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -25,6 +26,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 这个是用路由
 app.use('/', index);
 app.use('/users', users);
 app.use('/signin', signin);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Job' });
+  res.render('index', { title: 'Job' }); //render 一个jade文件
 });
 
 module.exports = router;
