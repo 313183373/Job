578839b3294b77100759cc7b683a71a3aa612335
熊鑫 <xiongxin@xiongxindeMacBook-Pro.local>
2017-09-18 23:11:31 +0800

    finish post job

--- routes/postJob.js
+++ routes/postJob.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+
+let Job=require('../model/user').Job;
 
 /* GET Post Job page. */
 router.get('/', function(req, res, next) {
     res.render('postJob');
+});
+
+
+router.post('/create_job',function (req, res) {
+    let workplace=(req.body.oCountry?req.body.oCountry:'')+(req.body.oCity?req.body.oCity:'')+(req.body.prov?req.body.prov:'')+(req.body.city?req.body.city:'')+(req.body.street?req.body.street:'');
+    console.log(workplace);
+    (async ()=>{
+        let result= await Job.create({
+            userEid:req.body.email,
+            title:req.body.title,
+            company:req.body.company,
+            jobdesc:req.body.desc,
+            proposal:req.body.apply,
+            deadline:req.body.deadline,
+            position:req.body.category,
+            nature:req.body.type,
+            tags:req.body.tags,
+            workplace:workplace,
+            publish:0
+        });
+        if(result){
+            res.end(JSON.stringify(result));
+        }else{
+            res.end('error');
+        }
+    })();
 });
 
 module.exports = router;
