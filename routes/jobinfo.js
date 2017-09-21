a644df15f20adb93697603f0338872c3eae42a5a
313183373 <313183373@qq.com>
2017-09-21 23:08:58 +0800

    update index and job info

--- routes/jobinfo.js
+++ routes/jobinfo.js
@@ -4,7 +4,18 @@ let router = express.Router();
 let Job=require('../model/user').Job;
 
 router.get('/', function (req, res, next) {
-    res.render('jobinfo');
+    let jid=req.query.id;
+    (async ()=>{
+        let job=await Job.findAll({
+            where:{
+                jid:jid
+            }
+        });
+        job=job[0];
+        console.log(job);
+        res.render('jobinfo',{job_info:job});
+    })();
 });
+
 
 module.exports = router;
