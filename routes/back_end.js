675a4b14b66898051b35dc4999c9f652cb929201
313183373 <313183373@qq.com>
2017-10-25 18:00:44 +0800

    add search users

--- routes/back_end.js
+++ routes/back_end.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let router = express.Router();
+let mysql =require('mysql');
 let DB = require('../model/user.js');
 let User = DB.User;
 let Job = DB.Job;
@@ -36,4 +37,54 @@ router.post('/get_user_number', function (req, res) {
     })();
 });
 
+router.post('/searchUser',function (req, res) {
+    (async ()=>{
+        let text=req.body.text;
+        let words=resolve(text);
+        let url='select * from users where (';
+        for (let i = 0; i < words.length; i++) {
+            url += `eid like '${words[i]}' OR password like '${words[i]}' OR cname like '${words[i]}' OR cpro like '${words[i]}' OR cadd like '${words[i]}'`;
+            if (words[i + 1]) {
+                url += ' OR ';
+            }
+        }
+        url += `)`;
+
+        let connection = mysql.createConnection({
+            host: 'rm-uf666l9f181m30sa1o.mysql.rds.aliyuncs.com',
+            user: 'www',
+            password: 'www',
+            database: 'test',
+            port: '3306'
+        });
+
+        connection.connect(function (err) {
+            if (err) {
+                console.log('error connecting:' + err.stack);
+                return;
+            }
+            console.log('connected as id ' + connection.threadId);
+        });
+
+        //2.查询
+        connection.query(url, function (err, rows, fields) {
+            if (err) throw err;
+            console.log('selected！');
+            // rows=rows.map((val,index,arr)=>{
+            //     return val.jid;
+            // });
+            res.end(JSON.stringify(rows));
+        });
+
+        //3.关闭数据库
+        connection.end();
+    })();
+});
+
+function resolve(str) {
+    let words = str.trim().split(/\s/);
+    words = words.filter(function (word) { return word !== '' });
+    words = words.map(function (word) { return word = `%${word}%` });
+    return words;
+}
 module.exports = router;
